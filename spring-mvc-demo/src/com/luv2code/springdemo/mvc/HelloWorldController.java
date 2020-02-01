eeaaac8b938252815b5d5d8c55510cd88a24f84d
SASHA <SASHA@GAZ>
2020-02-01 08:31:52 +0200

    120_lesson_BindingRequestParams

--- spring-mvc-demo/src/com/luv2code/springdemo/mvc/HelloWorldController.java
+++ spring-mvc-demo/src/com/luv2code/springdemo/mvc/HelloWorldController.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
@@ -31,6 +32,20 @@ public class HelloWorldController {
 		name = name.toUpperCase();
 		//create the message
 		String message = "Yo "+name;
+		
+		//add message to model
+		model.addAttribute("message", message);
+		
+		return "helloworld";
+	}
+	
+	@RequestMapping("/processFormVersionThree")
+	public String processFormVersionThree(@RequestParam("studentName")String name, Model model) {
+		
+		//convert the data to all caps
+		name = name.toUpperCase();
+		//create the message
+		String message = "Hey My Friend from v3 "+name;
 		
 		//add message to model
 		model.addAttribute("message", message);
