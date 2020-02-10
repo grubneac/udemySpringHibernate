7a77cad8c7aab805a05f419165557fb1770a1b59
SASHA <SASHA@GAZ>
2020-02-10 20:16:03 +0200

    189_lesson_Hibernate_PrimaryKey_IDENTITY

---
+++ hibernate-tutorial/src/com/luv2code/hibernate/demo/entity/PrimaryKeyDemo.java
@@ -0,0 +1,40 @@
+package com.luv2code.hibernate.demo.entity;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class PrimaryKeyDemo {
+
+	public static void main(String[] args) {
+		SessionFactory factory = new Configuration().
+											configure().
+											addAnnotatedClass(Student.class).
+											buildSessionFactory();
+							
+		Session session = factory.getCurrentSession();
+		
+		try{
+		//create 3 students object
+		Student st = new Student("firstName", "lastName", "email");
+		Student st1 = new Student("firstName1", "lastName1", "email1");
+		Student st2 = new Student("firstName2", "lastName2", "email2");
+		
+		//start transaction
+		session.beginTransaction();
+		
+		//save the student
+		session.save(st);
+		session.save(st1);
+		session.save(st2);
+		
+		//commit the transaction
+		session.getTransaction().commit();
+		
+		}finally{
+		factory.close();
+		}		
+		
+	}
+
+}
