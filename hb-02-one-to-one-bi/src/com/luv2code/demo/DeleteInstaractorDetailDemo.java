5e68462b5e8e531a30e7ecacfd9faaf7803e86ea
SASHA <SASHA@GAZ>
2020-02-18 19:11:43 +0200

    219_lesson_Hibernate_Bi-Direct_DeleteCascade

---
+++ hb-02-one-to-one-bi/src/com/luv2code/demo/DeleteInstaractorDetailDemo.java
@@ -0,0 +1,43 @@
+package com.luv2code.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.luv2code.hibernate.demo.entity.Instructor;
+import com.luv2code.hibernate.demo.entity.InstructorDetail;
+
+public class DeleteInstaractorDetailDemo {
+
+	public static void main(String[] args) {
+		SessionFactory factory = new Configuration().
+										configure().
+										addAnnotatedClass(Instructor.class).
+										addAnnotatedClass(InstructorDetail.class).
+										buildSessionFactory();
+		
+		Session session = factory.getCurrentSession();
+		
+		try{
+		
+			//start transaction
+			session.beginTransaction();
+			
+			InstructorDetail instructorDetail =
+					session.get(InstructorDetail.class, 3);
+			System.out.println("instructorDetail "+instructorDetail);
+			System.out.println("instractor "+instructorDetail.getInstructor());
+			
+			session.delete(instructorDetail);
+			//commit the transaction
+			session.getTransaction().commit();
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally{
+			session.close();
+			factory.close();
+		}		
+
+	}
+
+}
