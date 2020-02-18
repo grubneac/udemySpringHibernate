ac583c35fee71b88ba4901fb4caf900daa8b6d7b
SASHA <SASHA@GAZ>
2020-02-18 20:58:17 +0200

    231_lesson_Hibernate_One2Many_Delete_Course

---
+++ hb-03-one-to-many/src/com/luv2code/demo/DeleteCoursesDemo.java
@@ -0,0 +1,48 @@
+package com.luv2code.demo;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.luv2code.hibernate.demo.entity.Course;
+import com.luv2code.hibernate.demo.entity.Instructor;
+import com.luv2code.hibernate.demo.entity.InstructorDetail;
+
+public class DeleteCoursesDemo {
+
+	public static void main(String[] args) {
+		SessionFactory factory = new Configuration().
+										configure().
+										addAnnotatedClass(Instructor.class).
+										addAnnotatedClass(InstructorDetail.class).
+										addAnnotatedClass(Course.class).
+										buildSessionFactory();
+		
+		Session session = factory.getCurrentSession();
+		
+		try{
+			
+			
+			
+			//start transaction
+			session.beginTransaction();
+			
+			Course course = session.get(Course.class, 10);
+			System.out.println("Delete "+course);
+			
+			session.delete(course);
+			
+			
+			//commit the transaction
+			session.getTransaction().commit();
+			System.out.println("Done!");
+		}finally{
+			session.close();
+			factory.close();
+		}		
+
+	}
+
+}
