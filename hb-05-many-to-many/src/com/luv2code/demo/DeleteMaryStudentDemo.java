756074acb17378c3490459874afd898b9eb3b8f0
SASHA <SASHA@GAZ>
2020-02-24 19:35:38 +0200

    Delete Student

---
+++ hb-05-many-to-many/src/com/luv2code/demo/DeleteMaryStudentDemo.java
@@ -0,0 +1,50 @@
+package com.luv2code.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.luv2code.hibernate.demo.entity.Course;
+import com.luv2code.hibernate.demo.entity.Instructor;
+import com.luv2code.hibernate.demo.entity.InstructorDetail;
+import com.luv2code.hibernate.demo.entity.Review;
+import com.luv2code.hibernate.demo.entity.Student;
+
+public class DeleteMaryStudentDemo {
+
+	public static void main(String[] args) {
+		SessionFactory factory = new Configuration().
+										configure().
+										addAnnotatedClass(Instructor.class).
+										addAnnotatedClass(InstructorDetail.class).
+										addAnnotatedClass(Course.class).
+										addAnnotatedClass(Review.class).
+										addAnnotatedClass(Student.class).
+										buildSessionFactory();
+		
+		Session session = factory.getCurrentSession();
+		
+		try{
+			
+			
+			
+			//start transaction
+			session.beginTransaction();
+			
+			//get mary from database
+			Student theMary =session.get(Student.class, 2);
+			System.out.println("Deleting student: "+theMary);
+			
+			session.delete(theMary);
+			
+				//commit the transaction
+			session.getTransaction().commit();
+			System.out.println("Done!");
+		}finally{
+			session.close();
+			factory.close();
+		}		
+
+	}
+
+}
