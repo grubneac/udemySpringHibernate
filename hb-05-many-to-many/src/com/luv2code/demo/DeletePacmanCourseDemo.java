4c25c259d07f477b4acb1330866b5d4ada24bef4
SASHA <SASHA@GAZ>
2020-02-24 19:30:17 +0200

    Delete the course

---
+++ hb-05-many-to-many/src/com/luv2code/demo/DeletePacmanCourseDemo.java
@@ -0,0 +1,50 @@
+package com.luv2code.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.luv2code.hibernate.demo.entity.Course;
+import com.luv2code.hibernate.demo.entity.Instructor;
+import com.luv2code.hibernate.demo.entity.InstructorDetail;
+import com.luv2code.hibernate.demo.entity.Review;
+import com.luv2code.hibernate.demo.entity.Student;
+
+public class DeletePacmanCourseDemo {
+
+	public static void main(String[] args) {
+		SessionFactory factory = new Configuration().
+										configure().
+										addAnnotatedClass(Instructor.class).
+										addAnnotatedClass(InstructorDetail.class).
+										addAnnotatedClass(Course.class).
+										addAnnotatedClass(Review.class).
+										addAnnotatedClass(Student.class).
+										buildSessionFactory();
+		
+		Session session = factory.getCurrentSession();
+		
+		try{
+			
+			
+			
+			//start transaction
+			session.beginTransaction();
+			
+			//get course
+			Course thePacman =session.get(Course.class, 10);
+			
+			System.out.println("Deleting the course "+thePacman.getTitle());
+			session.delete(thePacman);
+			
+				//commit the transaction
+			session.getTransaction().commit();
+			System.out.println("Done!");
+		}finally{
+			session.close();
+			factory.close();
+		}		
+
+	}
+
+}
