230ad093fd18d23e2d31888d9b26582eb2d521cd
SASHA <SASHA@GAZ>
2020-02-26 19:49:59 +0200

    add annotation

--- web-customer-tracker/src/com/luv2code/springdemo/dao/CustomerDAOImpl.java
+++ web-customer-tracker/src/com/luv2code/springdemo/dao/CustomerDAOImpl.java
@@ -5,10 +5,12 @@ import java.util.List;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.luv2code.springdemo.entity.Customer;
 
+@Repository
 public class CustomerDAOImpl implements CustomerDAO {
 
 	//need inject the session factory
