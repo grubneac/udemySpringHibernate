7640ba7c89b5663309fa87d098b3bb0b1dcb7e5d
SASHA <SASHA@GAZ>
2020-03-13 19:16:44 +0200

    add hidden property id on customer-form

--- web-customer-tracker/src/com/luv2code/springdemo/dao/CustomerDAOImpl.java
+++ web-customer-tracker/src/com/luv2code/springdemo/dao/CustomerDAOImpl.java
@@ -36,7 +36,7 @@ public class CustomerDAOImpl implements CustomerDAO {
 	public void save(Customer customer) {
 		Session session = sessionFactory.getCurrentSession();
 		
-		session.save(customer);
+		session.saveOrUpdate(customer);
 		
 	}
 
