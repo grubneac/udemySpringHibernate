03efecf44596c1742d61e993d0ff023c49f17ef6
SASHA <SASHA@GAZ>
2020-04-27 20:44:15 +0300

    ignore unknown property from JSON

--- jackson-databinding-json-demo/src/main/java/com/luv2code/jackson/json/demo/Student.java
+++ jackson-databinding-json-demo/src/main/java/com/luv2code/jackson/json/demo/Student.java
@@ -2,6 +2,9 @@ package com.luv2code.jackson.json.demo;
 
 import java.util.Arrays;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Student {
 	
 	private int id;
