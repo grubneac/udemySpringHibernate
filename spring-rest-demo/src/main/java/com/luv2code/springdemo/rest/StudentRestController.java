62d47b6abc4867eebf237f85f951af20e8a174ec
SASHA <SASHA@GAZ>
2020-05-01 13:25:09 +0300

    @PathVarible example

--- spring-rest-demo/src/main/java/com/luv2code/springdemo/rest/StudentRestController.java
+++ spring-rest-demo/src/main/java/com/luv2code/springdemo/rest/StudentRestController.java
@@ -3,7 +3,10 @@ package com.luv2code.springdemo.rest;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,14 +16,25 @@ import com.luv2code.springdemo.entity.Student;
 @RequestMapping("/api")
 public class StudentRestController {
 	
+	private List<Student> theStudents;
+	
+	@PostConstruct
+	private void initListStudents() {
+		theStudents = new ArrayList<>();
+		
+		theStudents.add(new Student("Poornima", "Patel"));
+		theStudents.add(new Student("Mario", "Rossi"));
+		theStudents.add(new Student("Mary", "Smith"));
+	}
+	
 	@GetMapping("/list")
 	public List<Student> getStudents(){
-		List<Student> result = new ArrayList<>();
-		result.add(new Student("Poornima", "Patel"));
-		result.add(new Student("Mario", "Rossi"));
-		result.add(new Student("Mary", "Smith"));
-		
-		return result;
+		return theStudents;
+	}
+
+	@GetMapping("/list/{studentId}")
+	public Student getStudentsById(@PathVariable int studentId){
+		return theStudents.get(studentId);
 	}
 
 }
