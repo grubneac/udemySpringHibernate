f3010d82e60c0edc2d11a30f71e38599f0b21bd9
SASHA <SASHA@GAZ>
2020-05-03 08:04:54 +0300

    497 add customer

--- spring-crm-rest-demo/src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
+++ spring-crm-rest-demo/src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,11 +20,14 @@ public class CustomerRestController {
 	@Autowired
 	CustomerService customerService;
 	
+	//select all customers
 	@GetMapping("/customers")
 	public List<Customer> getCustomers(){
 		
 		return customerService.getCustomers();
 	}
+	
+	//select on costomer
 	@GetMapping("/customers/{customerId}")
 	public Customer getCustomer(@PathVariable int customerId){
 		
@@ -31,6 +36,18 @@ public class CustomerRestController {
 		if(theCustomer == null)
 			throw new CustomerNotFoundException("Customer id not found - " + customerId);
 		
+		
+		return theCustomer;
+	}
+	
+	//insert new customer
+	@PostMapping("/customers")
+	public Customer saveCustomer(@RequestBody Customer theCustomer) {
+		
+		//explain service that add new records 
+		theCustomer.setId(0);
+		
+		 customerService.saveCustomer(theCustomer);
 		
 		return theCustomer;
 	}
