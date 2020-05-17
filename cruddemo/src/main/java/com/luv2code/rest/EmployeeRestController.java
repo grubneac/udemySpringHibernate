784a9ae4fdc9b174f1f302f443e162b44070fa98
SASHA <SASHA@GAZ>
2020-05-17 22:10:16 +0300

    529 add rest api: update, delete

--- cruddemo/src/main/java/com/luv2code/rest/EmployeeRestController.java
+++ cruddemo/src/main/java/com/luv2code/rest/EmployeeRestController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -46,6 +48,19 @@ public class EmployeeRestController {
 		theEmployee.setId(0);//add new record
 		employeeService.save(theEmployee);
 		return theEmployee;
+	}
+	@PutMapping("/employees")
+	public Employee updateEmployee(@RequestBody Employee theEmployee){
+		employeeService.save(theEmployee);
+		return theEmployee;
+	}
+	@DeleteMapping("/employees/{employeeId}")
+	public String deleteEmployee(@PathVariable int employeeId){
+		Employee theEmployee = employeeService.findById(employeeId);
+		if(theEmployee == null)
+			throw new RuntimeException("Employee id not found - "+ employeeId);
+		employeeService.delete(employeeId);
+		return "Deleted employee id - "+employeeId;
 	}
 	
 }
