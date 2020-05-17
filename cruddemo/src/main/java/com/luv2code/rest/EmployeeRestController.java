35d2af960a2310bc99521645b2bee49f457c00cb
SASHA <SASHA@GAZ>
2020-05-17 21:46:41 +0300

    528 add rest api: getById, add

--- cruddemo/src/main/java/com/luv2code/rest/EmployeeRestController.java
+++ cruddemo/src/main/java/com/luv2code/rest/EmployeeRestController.java
@@ -3,7 +3,11 @@ package com.luv2code.rest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,6 +29,23 @@ public class EmployeeRestController {
 	@GetMapping("/employees")
 	public List<Employee> findAll(){
 		return employeeService.findAll();
+	}
+
+	@GetMapping("/employees/{employeeId}")
+	public Employee getEmployee(@PathVariable int employeeId){
+		Employee theEmployee = employeeService.findById(employeeId);
+		if(theEmployee == null)
+			throw new RuntimeException("Employee id not found - "+ employeeId);
+		
+		return theEmployee;
+		
+	}
+	
+	@PostMapping("/employees")
+	public Employee addEmployee(@RequestBody Employee theEmployee){
+		theEmployee.setId(0);//add new record
+		employeeService.save(theEmployee);
+		return theEmployee;
 	}
 	
 }
