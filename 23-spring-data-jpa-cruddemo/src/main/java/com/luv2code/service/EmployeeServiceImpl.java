25fd51a7a95a43f1c3e4cb88d8c4b3598514364f
SASHA <SASHA@GAZ>
2020-05-19 20:44:42 +0300

    534  Spring-DATA  Spring JPA Repository

---
+++ 23-spring-data-jpa-cruddemo/src/main/java/com/luv2code/service/EmployeeServiceImpl.java
@@ -0,0 +1,57 @@
+package com.luv2code.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.luv2code.dao.EmployeeRepository;
+import com.luv2code.entity.Employee;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+	
+	private EmployeeRepository employeeRepository;
+	
+	@Autowired
+	
+	public EmployeeServiceImpl(EmployeeRepository theEmployeeRepository) {
+		this.employeeRepository = theEmployeeRepository;
+	}
+
+	@Override
+	public List<Employee> findAll() {
+		return employeeRepository.findAll();
+	}
+
+	@Override
+	public Employee findById(int theId) {
+		
+		Optional<Employee> findById = employeeRepository.findById(theId);
+		
+		Employee theEmployee = null;
+		
+		if(findById.isPresent())
+			theEmployee= findById.get();
+		else
+			throw new RuntimeException("Did not find employee id - "+ theId);
+		
+		return theEmployee;
+	}
+
+	@Override
+	public void save(Employee theEmployee) {
+		employeeRepository.save(theEmployee);
+
+	}
+
+	@Override
+	public void delete(int theId) {
+		employeeRepository.deleteById(theId);
+
+	}
+
+}
