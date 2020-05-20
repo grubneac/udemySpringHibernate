39444788ce8d8ce4f9273ee58718fb8150b814fa
SASHA <SASHA@GAZ>
2020-05-20 19:40:08 +0300

    add properties to Spring data rest

--- 24-spring-data-REST-cruddemo/src/main/java/com/luv2code/dao/EmployeeRepository.java
+++ 24-spring-data-REST-cruddemo/src/main/java/com/luv2code/dao/EmployeeRepository.java
@@ -1,9 +1,11 @@
 package com.luv2code.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.luv2code.entity.Employee;
 
+@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 }
